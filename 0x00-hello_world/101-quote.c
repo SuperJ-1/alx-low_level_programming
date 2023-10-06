63ac2885732765d265d64aa081d702327f9f16ed
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-05 21:52:23 -0700

    task 8 using write command

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *Main sets the start up for this code
+ *
+ *Return is where this code ends
+ */
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
