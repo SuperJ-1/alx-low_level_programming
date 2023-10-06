baac928c8231a6a40d7bc00db7ec489a16fb13ef
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-05 20:53:12 -0700

    print with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *Main - starting block
+ *
+ * Returns: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
