0725a3419962a9a37c130a1a732cbb667d9f476f
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-05 20:43:43 -0700

    c program that prints with puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *Main- is where the code begins
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+
+}
