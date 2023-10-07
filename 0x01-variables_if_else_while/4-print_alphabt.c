da894ee80c2f11c6b6b5e3e37f8125fbcbb5f4f6
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-07 07:29:30 -0700

    prints alphabets without q & e"

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,13 @@
  */
 int main(void)
 {
-	for (char ch = 'a'; ch <= 'z'; ch++)
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch != 'e' && ch != 'q')
 			putchar(ch);
 	}
 	putchar('\n');
 	return (0);
+}
