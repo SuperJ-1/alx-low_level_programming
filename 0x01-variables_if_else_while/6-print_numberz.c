3a4c3c4f00c50177fc202a21e411dc78be1f9a44
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-07 11:01:10 -0700

    prints all single digits in base 10:

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  *
+ * Description: prints all single digits in base 10
  *
- *
- *
- * return: 0
+ * Return: returns 0
  */
 int main(void)
 {
-	putchar('0');
+	char sdinb10 = 0;
+
+	while (sdinb10 <= 10)
+		;
+	{
+		putchar(sdinb10);
+		sdinb10++;
+	}
+	putchar('\n');
+	return (0);
+}
