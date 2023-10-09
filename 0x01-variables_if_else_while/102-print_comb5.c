ccfe123566ca66275a16877eef4df55515b987c5
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-08 19:42:18 -0700

    prints combinations from 0 to 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,25 +11,21 @@ int main(void)
 {
 	int n1, n2;
 
-	for (n1 = 0; n1 < 100; n1++)
+	for (n1 = 0; n1 <= 98; n1++)
 	{
-		for (n2 = n1; n2 < 100; n2++)
+		for (n2 = n1 + 1; n2 <= 99; n2++)
 		{
-			int num1 = n1 / 10;
-			int num2 = n1 % 10;
-			int num3 = n2 / 10;
-			int num4 = n2 % 10;
-
-			putchar(num1 + '0');
-			putchar(num2 + '0');
+			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
 			putchar(' ');
-			putchar(num3 + '0');
-			putchar(num4 + '0');
-			if (!(n1 == 99 && n2 == 99))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar((n2 / 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n1 == 98 && n2 == 99)
+				continue;
+	
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	return (0);
