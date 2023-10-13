68d9acac966b0b8e9a754c195018b8c608430e19
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-12 21:35:11 -0700

    prints numbers 0 to 9

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _isuper - Entry point, expressed in main.h
- *
- * Description: checks for uppercase character
+ *_isuper - Entry point, expressed in main.h
  *
  * @c: character to check
+ *
+ * Description: checks for uppercase character
  *
  * Return: returns 0 or 1 if positive
  */
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - find the product of 2 integers
+ *
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Returns: the product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
