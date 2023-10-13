cd879bc79822c204eb5e9b0698be526c5ef91823
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-12 21:24:04 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isuper - Entry point, expressed in main.h
+ *
+ * Description: checks for uppercase character
+ *
+ * @c: character to check
+ *
+ * Return: returns 0 or 1 if positive
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+
+	return (0);
+}
