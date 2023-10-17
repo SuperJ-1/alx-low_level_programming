9198f689a06c24e9e8555a5b075178528b87c6dd
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-16 20:52:34 -0700

    copies string pointer from src to a dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - start point
+ * @dest: where to copy the string
+ * @src: source of the string to copy
+ *
+ * Desc: copies a string to a pointed destination
+ *
+ * Return: pointe to the dest string
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
