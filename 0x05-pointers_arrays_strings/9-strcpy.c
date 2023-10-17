1f1b4ac1797f8cedadc823c72b33a2996c027100
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-10-17 08:48:37 -0700

    copies the string pointed to a src to a set dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,19 +3,27 @@
  * _strcpy - start point
  * @dest: where to copy the string
  * @src: source of the string to copy
- *
+ * @n: bytes to copy
  * Desc: copies a string to a pointed destination
  *
  * Return: pointe to the dest string
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src, int n)
 {
-	int index = 0;
+	int i;
 
-	while (src[index])
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
