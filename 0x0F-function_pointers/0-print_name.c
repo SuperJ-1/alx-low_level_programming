1aca2fabee17e4f587739cabaa5c55f822e128b6
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-11-08 17:43:21 -0800

    using function pointer to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "func_pointers.h"
+/**
+ * print_name - to print a name
+ * @name: name to be printed
+ * @f: a function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
