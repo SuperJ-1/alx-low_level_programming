6dc232767fc0a005aa40c1dab87c16fbd11fcd0b
JERRY BABATUNDE <jerrybabatunde1@gmail.com>
2023-12-23 09:44:02 -0800

    function to append text to the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			length++;
 	}
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, lenght);
+	w = write(o, text_content, length);
 
 	if (o == -1 || w == -1)
 		return (-1);
